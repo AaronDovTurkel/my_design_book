3423d320e820d6620ab3651acc6837de3c218965
Aaron Dov Turkel <AaronDovTurkel@gmail.com>
2019-02-23 20:07:53 -0500

    added profile schema

--- models.js
+++ models.js
@@ -11,12 +11,35 @@ const accountSchema = mongoose.Schema({
   email: {type: String, required: true},
   accountType: {type: String, required: true},
   userName: {type: String, required: true},
-  passWord: {type: String, required: true}
+  passWord: {type: String, required: true}, 
+  taggedAccounts: {type: String, required: false},
+  profile: {type: String, required: false}
 });
 
+const profileSchema = mongoose.Schema({
+  gender: {type: String, required: true},
+  dob: {type: String, required: true},
+  address: {
+    streetAddress: {type: String, required: false},
+    city: {type: String, required: false},
+    state: {type: String, required: false},
+    zipCode: {type: String, required: false}
+  },
+  designStyle: {type: String, required: false},
+  profileImage: {type: String, required: false},
+  personalInfo: {type: String, required: false}
+});
 
 accountSchema.virtual('accountName').get(function() {
   return `${this.name.firstName} ${this.name.lastName}`.trim();
+});
+
+profileSchema.virtual('profileAddress').get(function() {
+  return `${this.address.streetAddress}, ${this.address.city}, ${this.address.state}, ${this.address.zipCode}`.trim();
+});
+
+profileSchema.virtual('profileLocation').get(function() {
+  return `${this.address.state}`.trim();
 });
 
 accountSchema.methods.serialize = function() {
@@ -28,6 +51,17 @@ accountSchema.methods.serialize = function() {
   };
 };
 
+profileSchema.methods.serialize = function() {
+  return {
+    profileImage: this.profileImage,
+    designStyle: this.designStyle,
+    personalInfo: this.personalInfo,
+    location: this.profileLocation
+  };
+};
+
 const Account = mongoose.model('Account', accountSchema);
 
-module.exports = { Account };
+const Profile = mongoose.model('Profile', profileSchema);
+
+module.exports = { Account, Profile };
